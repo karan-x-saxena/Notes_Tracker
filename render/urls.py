0cdcd390ae4f9952d68bf5c74ab0b9db8c12409b
Karan Saxena <karan.saxena@zenatix.com>
2024-11-17 17:42:21 +0530

    Add login url route

--- render/urls.py
+++ render/urls.py
@@ -1,8 +1,10 @@
 # myapp/urls.py
+from django.shortcuts import redirect
 from django.urls import path
 from render.views import login_page, test_blog
 
 urlpatterns = [
+    path('', lambda request: redirect('/login_page', permanent=True)),
     path('login_page/', login_page, name='login_page'),
     path('test_blog/', test_blog, name='test_blog'),
 ]
